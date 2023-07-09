b719b2d5b7080c708a8f243710134240cf3ec620
Sai <saishankar15052005@gmail.com>
2023-07-09 16:55:07 +0530

    updated navbar and added animations

---
+++ script.js
@@ -0,0 +1,28 @@
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarSupportedContent')
+const bsCollapse = new bootstrap.Collapse(menuToggle, {toggle:false})
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { bsCollapse.toggle() })
+})
+
+
+window.addEventListener('load', function() {
+    var animatedElements = document.getElementsByClassName('animated-content-top');
+    for (var i = 0; i < animatedElements.length; i++) {
+      animatedElements[i].classList.add('animate');
+    }
+  });
+
+window.addEventListener('load', function() {
+    var animatedElements = document.getElementsByClassName('animated-content-left');
+    for (var i = 0; i < animatedElements.length; i++) {
+      animatedElements[i].classList.add('animate');
+    }
+  });
+
+window.addEventListener('load', function() {
+    var animatedElements = document.getElementsByClassName('animated-content-right');
+    for (var i = 0; i < animatedElements.length; i++) {
+      animatedElements[i].classList.add('animate');
+    }
+  });
